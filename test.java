2052e2326fc7a8a3782b3f6bf36b50983edd409f
Shubham Maini <shubhammaini@192-168-1-104.tpgi.com.au>
2022-10-22 10:03:48 +1100

    first commit

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class FirstClass {
+    public static void main(String args[]) {
+        // variables
+
+        Scanner sc = new Scanner(System.in);
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // String name = sc.nextLine();
+        // // nextInts
+
+        // System.out.println(sum);
+    }
+}
